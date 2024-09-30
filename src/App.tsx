3f6ee239d26634a1ec0e0d1a21115556f5b8a34a
Shalom Gispan <shalomgispan1@gmail.com>
2024-09-30 10:34:55 +0300

    try gispan

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import Home from './pages/home/Home';
 
 export default function App() {
   return (
-
+<>
     <div className="bg-blue-500 text-white p-4">
       <h1 className="text-4xl font-bold">Hello Project !</h1>
       <Home/>
     </div>
+    <br />
+    <h1 className="text-orange-600 bg-blue-600 ">Try</h1>
+    </>
   );
 }
