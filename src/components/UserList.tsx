a3d211ff93b96e5d926e83c869985ac626960a0a
Oshrat Krakover <oshrat@msbitsoftware.com>
2024-10-07 15:02:31 +0300

    kislev

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,79 +1,103 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import ConfirmationDialogue from './ConfirmationDialogue';
-import EditButton from './EditButton';
+import ConfirmationDialogue from "./ConfirmationDialogue";
 
-import ExportButton from './ExportButton';
+import EditButton from "./EditButton";
 
-
-
-
+import ExportButton from "./ExportButton";
 
 interface User {
-    _id: string;
-    firstName: string;
-    lastName: string;
-    phone: string;
-    email: string;
-    role: string;
-    password: string;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  role: string;
+  password: string;
+}
+
+interface EditButtonProps {
+  user: User;
 }
 
 const UsersList: React.FC = () => {
-    const [dataBase, setDataBase] = useState<User[]>([]);
+  const [dataBase, setDataBase] = useState<User[]>([]);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('http://localhost:3000/users/getAllUsers');
-            const users: User[] = await response.json();
-            setDataBase(users);
-        } catch (error) {
-            console.log('Error: ', error);
-        }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/users/getAllUsers");
+      const users: User[] = await response.json();
+      setDataBase(users);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <>
-        <ExportButton/>
-            <h1 className="text-xl font-bold mb-4">נתוני משתמשים</h1>
-            <table className="w-full border-collapse text-right ">
-                <thead>
-                    <tr>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">פעולות</th>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">סיסמה</th>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">תפקיד</th>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">דואר אלקטרוני</th>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">טלפון נייד</th>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">שם משפחה</th>
-                        <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">שם</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataBase.map((user) => (
-                        <tr key={user._id} className={`hover:bg-gray-300  'bg-white'}`}>
-                            <td className="border border-gray-300 p-2 text-center">
-                                <div className="flex justify-center space-x-2">
-                                    <ConfirmationDialogue />
-                                    <EditButton />
-                                </div>
-                            </td>
-                            <td className="border border-gray-300 p-2 text-right">******</td>
-                            <td className="border border-gray-300 p-2 text-right">{user.role}</td>
-                            <td className="border border-gray-300 p-2 text-right">{user.email}</td>
-                            <td className="border border-gray-300 p-2 text-right">{user.phone}</td>
-                            <td className="border border-gray-300 p-2 text-right">{user.lastName}</td>
-                            <td className="border border-gray-300 p-2 text-right">{user.firstName}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </>
-    );
+  return (
+    <>
+      <ExportButton />
+      <h1 className="text-xl font-bold mb-4">נתוני משתמשים</h1>
+      <table className="w-full border-collapse text-right ">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              פעולות
+            </th>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              סיסמה
+            </th>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              תפקיד
+            </th>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              דואר אלקטרוני
+            </th>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              טלפון נייד
+            </th>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              שם משפחה
+            </th>
+            <th className="border border-gray-300 bg-gray-200 font-bold text-right p-2">
+              שם
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataBase.map((user) => (
+            <tr key={user._id} className={`hover:bg-gray-300  'bg-white'}`}>
+              <td className="border border-gray-300 p-2 text-center">
+                <div className="flex justify-center space-x-2">
+                  <ConfirmationDialogue />
+                  <EditButton user={user} />
+                </div>
+              </td>
+              <td className="border border-gray-300 p-2 text-right">******</td>
+              <td className="border border-gray-300 p-2 text-right">
+                {user.role}
+              </td>
+              <td className="border border-gray-300 p-2 text-right">
+                {user.email}
+              </td>
+              <td className="border border-gray-300 p-2 text-right">
+                {user.phone}
+              </td>
+              <td className="border border-gray-300 p-2 text-right">
+                {user.lastName}
+              </td>
+              <td className="border border-gray-300 p-2 text-right">
+                {user.firstName}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default UsersList;
-
