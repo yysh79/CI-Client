9de2a289c35783c93df65159d537b65c8b40a0e0
itzik <yizohar770@gmail.com>
2024-10-08 12:57:46 +0300

    delete

--- src/components/ConfirmationDialogue.tsx
+++ src/components/ConfirmationDialogue.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
+import Delete from './delete';
 
 const ConfirmationDialogue: React.FC = () => {
     const [showDiv, setShowDiv] = useState<boolean>(false);
@@ -28,9 +29,7 @@ const ConfirmationDialogue: React.FC = () => {
                 <div ref={dialogRef} className="absolute z-10 bg-white border border-gray-300 shadow-lg rounded-lg p-4 mt-2 w-64">
                     <p className="text-gray-800">אתה בטוח שברצונך למחוק את המשתמש ? </p>
                     <div className="mt-4 flex justify-end space-x-2">
-                        <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
-                            כן
-                        </button>
+                       <Delete/>
                         <button
                             className="bg-gray-300 text-gray-800 px-4 py-2 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
                             onClick={leave}
---
+++ src/components/Delete.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Delete: React.FC = () => {
+    
+    return (
+        <>
+        <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+            כן
+        </button>
+        </>
+    )
+}
+
+export default Delete
