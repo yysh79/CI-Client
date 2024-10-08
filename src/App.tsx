dc20dc6ee715458e3af9efcad255cf0e1714e16a
shimon3112 <shimonkeynan@gmail.com>
2024-10-08 14:14:08 +0300

    shimon 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css'
 import AppRoutes from './layout/AppRoutes';
 import store from './store/store';
 
-export default function App() {
+export  function App() {
   return (
     <Provider store={store}>
       <AppRoutes/>
