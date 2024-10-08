3800bebb157f0aa7b8f65b436fc95303bd1fb17c
yysh79 <yysh79@gmail.com>
2024-10-08 14:20:48 +0300

    login 02

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import { FaCircleUser } from "react-icons/fa6";
 import { useSelector, useDispatch } from 'react-redux';
 import { showSideBar } from '../store/sideBarSlice';
 import { AppDispatch } from '../store/store'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
@@ -13,7 +14,7 @@ const Header = () => {
   return (
     <div className="flex items-center bg-gray-300 text-black p-4">
       <div className="flex items-center space-x-2 p-2 bg-white rounded-lg shadow hover:bg-gray-100 transition duration-300">
-        <FaCircleUser className='text-gray-500' size={30} />
+       <Link to={"/logIn"}> <FaCircleUser  className='text-gray-500' size={30} /></Link>
         <div className="text-gray-700 font-semibold">התחברות</div>
       </div>
       <RxHamburgerMenu color='black' size={30} className="ml-auto hover:text-gray-500  transition duration-300" onClick={handleClick} />
--- src/layout/AppRoutes.tsx
+++ src/layout/AppRoutes.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Home from '../pages/home/Home'
 import UsersList from '../components/UserList'
+import LogIn from '../components/LogIn';
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Layout />} >
-                 <Route index element={<Home />} />
+                <Route index element={<Home />} />
+                <Route path='/logIn' element={<LogIn />} />
                  <Route path='/usersList' element={<UsersList />} />
                 </Route>
             </Routes>
