8aab88bfb3cddaca61ce871ab1ab968e7c7917cd
yysh79 <yysh79@gmail.com>
2024-10-14 10:39:53 +0300

    userStore 02

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { Link} from 'react-router-dom';
 import Otp from './Otp'
+import { logIn } from '../store/userSlice';
+import { AppDispatch } from '../store/store';
+import { useDispatch } from 'react-redux';
+
+
 function Login() {
+
 
     const [email, setEmail] = useState(''); 
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
     const [loading, setLoading] = useState(false);
+    const dispatch: AppDispatch = useDispatch();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -29,6 +36,7 @@ function Login() {
             if (response.ok) {
                 console.log('Login successful:', data);
                 setSuccessMessage('! התחברות בהצלחה');
+                dispatch(logIn())
                 setTimeout(() => {
                     setSuccessMessage('');
                 }, 3000);
@@ -41,6 +49,8 @@ function Login() {
             setLoading(false);
         }
        };
+
+
     return (
         <div className='flex justify-center items-center min-h-screen bg-[url("/images/background-login.jpg")] bg-cover bg-center'>
             <div className="bg-gray-200 bg-opacity-80 p-8 rounded-lg shadow-lg max-w-md w-full">
@@ -77,85 +87,18 @@ function Login() {
                     </button>
                 </form>
                    
-                   <button ><p  className={`w-full ${loading ? 'bg-gray-400' : 'bg-blue-500'} text-white py-3 px-4 rounded-md font-bold hover:bg-blue-600 transition duration-300 shadow-md`}>
+                <p  className={`w-full  'bg-gray-400' : 'bg-blue-500'} text-white py-3 px-4 rounded-md font-bold hover:bg-blue-600 transition duration-300 shadow-md`}>
                        <Link to={"/otp"}>סיסמא במייל</Link>
                    </p>
-                </button>
+               
                 <p className="mt-6 text-center text-sm text-gray-600">
                     אין לך חשבון? <a href="#" className="text-blue-500 hover:underline">צור חשבון</a>
                 </p>
             </div>
         </div>
-    )
-import React, { useState } from 'react';
-import { FaUser } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
+    )}
 
-function LogIn() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: ''
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-  };
-  const responseGoogle = (response) => {
-    console.log(response);
-  };
 
-  return (
-    <div className="flex justify-center items-center h-screen bg-[url('/images/background-login.jpg')] bg-cover bg-center">
-      <div className="h-auto w-[400px] bg-white rounded-lg shadow-lg p-8 bg-opacity-90">
-        <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">Log In</h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="relative">
-            <label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="name">
-              Name
-            </label>
-            <FaUser className="absolute left-3 top-10 text-gray-500" />
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="w-full pl-10 pr-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your name"
-            />
-          </div>
-          <div className="relative">
-            <label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="email">
-              Email
-            </label>
-            <MdEmail className="absolute left-3 top-10 text-gray-500" />
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full pl-10 pr-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your email"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-          >
-            Submit
-          </button>
-        </form>
-     
-      </div>
-    </div>
-  );
-}
-
-export default Login
-
-export default LogIn;
+export default Login;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,19 +7,20 @@ const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 function Sidebar() {
   const { isSideBarVisible } = useTypedSelector((store) => store.sideBar);
+  const { isLogIn } = useTypedSelector((store) => store.signIn);
 
   return (
     <div className="flex">
-      {/* Sidebar */}
+      
       <div 
         className={`bg-gray-800 text-white flex flex-col items-center justify-start pt-5 
         ${isSideBarVisible ? 'w-[200px]' : 'w-0'}
         transition-width duration-300 ease-in-out overflow-hidden`}>
-        <Link 
+       {isLogIn?<Link 
           className="text-2xl mb-5 hover:text-gray-300 transition duration-300 ease-in-out" 
           to={'/usersList'}>
           משתמשים
-        </Link>
+        </Link>:<></>} 
       </div>
     </div>
   );
