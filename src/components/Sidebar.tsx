78561c8f46e8da968bb9ffa1bb67b1eece607e90
itzik <yizohar770@gmail.com>
2024-10-14 14:32:49 +0300

    logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,7 +13,9 @@ function Sidebar() {
     <>
     {isSideBarVisible? <div className='flex-col pt-5 text-center ml-auto w-[200px] h-screen  bg-gray-500'>
       
-      <Link className='text-white text-2xl  hover:text-gray-300 transition duration-300' to={'/usersList'}>משתמשים</Link>
+      <Link className='text-white text-2xl  hover:text-gray-300 transition duration-300' to={'/usersList'}>
+      <img src="images/logo20.png" alt="Citizen Impact Logo" className="h-48 w-48 object-contain"/>
+      </Link>
     </div>: <></>}
     </>
     
