26e52881e901a24f0f4c709639cb0d8b737e025d
orel123 <oral.yosf.h@gmail.com>
2024-10-14 11:30:06 +0300

    otp

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -51,7 +51,7 @@ function Login() {
        };
 
 
-    return (
+       return (
         <div className='flex justify-center items-center min-h-screen bg-[url("/images/background-login.jpg")] bg-cover bg-center'>
             <div className="bg-gray-200 bg-opacity-80 p-8 rounded-lg shadow-lg max-w-md w-full">
                 <h1 className="text-center text-3xl font-bold text-gray-800 mb-6">התחברות</h1>
@@ -86,18 +86,20 @@ function Login() {
                         כניסה
                     </button>
                 </form>
-                   
-                   <button ><p  className={`w-full ${loading ? 'bg-gray-400' : 'bg-blue-500'} text-white py-3 px-4 rounded-md font-bold hover:bg-blue-600 transition duration-300 shadow-md`}>
+                
+                <button className="w-full bg-green-500 text-white py-3 px-4 rounded-md font-bold hover:bg-green-600 transition duration-300 shadow-md mt-4">
+                    <p className="text-center">
                        <Link to={"/otp"}>סיסמא במייל</Link>
-                   </p>
+                    </p>
                 </button>
+                
                 <p className="mt-6 text-center text-sm text-gray-600">
                     אין לך חשבון? <a href="#" className="text-blue-500 hover:underline">צור חשבון</a>
                 </p>
             </div>
         </div>
     )
-}
+}    
 
 export default Login
 
