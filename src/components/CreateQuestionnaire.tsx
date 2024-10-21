6d41b774d95f12d157933939947c869c6157bfac
itzik <yizohar770@gmail.com>
2024-10-21 22:05:55 +0300

    CreateQuestionnaire

--- src/components/CreateQuestionnaire.tsx
+++ src/components/CreateQuestionnaire.tsx
@@ -1,8 +1,19 @@
 import React, { useRef, useState, useEffect } from 'react';
+import TextInput from '../components/questionnaires-components/TextInput'
+import NumberInput from '../components/questionnaires-components/NumberInput'
+import Textarea from '../components/questionnaires-components/Textarea'
+import Button from '../components/questionnaires-components/Button'
+import Checkbox from '../components/questionnaires-components/Checkbox'
+import Radio from '../components/questionnaires-components/Radio'
+import Select from '../components/questionnaires-components/Select'
+
+
 
 const CreateQuestionnaire: React.FC = () => {
 
     const [showDiv, setShowDiv] = useState<Boolean>(false);
+    const [array, setArray] = useState<JSX.Element[]>([]);
+    const [inputString, setInputString] = useState<String>('')
     const divRef = useRef<HTMLDivElement>(null);
 
     const handleClickOutside = (event: MouseEvent) => {
@@ -18,24 +29,44 @@ const CreateQuestionnaire: React.FC = () => {
             <div className="bg-white p-6 rounded-lg  mt-16 border border-gray-800" style={{ width: '600px', height: '600px' }}>
                 <form action="" className='flex flex-col h-full overflow-y-auto'>
                     <div className='flex-1 flex flex-col items-center space-y-4 pb-4'>
-                        <input type="text" className='w-full border border-gray-600 rounded-lg p-2' placeholder="שאלה 1" />
-                        <input type="text" className='w-full border border-gray-600 rounded-lg p-2' placeholder="שאלה 2" />
-                        <input type="text" className='w-full border border-gray-600 rounded-lg p-2' placeholder="שאלה 3" />
-                        <input type="text" className='w-full border border-gray-600 rounded-lg p-2' placeholder="שאלה 4" />
-                        <input type="text" className='w-full border border-gray-600 rounded-lg p-2' placeholder="שאלה 5" />
-                        <input type="text" className='w-full border border-gray-600 rounded-lg p-2' placeholder="שאלה 6" />
+
+                        {array.map((value, index) => (
+                            <div key={index}>{value}</div>
+                        ))}
+
                         <i aria-label='Add' className="material-icons text-gray-300 mx-2 cursor-pointer hover:text-gray-400 text-6xl"
                             onClick={enter}>
                             add
                         </i>
                         {showDiv && (<div ref={divRef} className='bg-gray-200 rounded-lg p-4 border border-gray-400 w-96 flex justify-center space-x-6'>
-                            <i aria-label='Text Fields' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>text_fields</i>
-                            <i aria-label='Pin' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>pin</i>
-                            <i aria-label='Segment' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>segment</i>
-                            <i aria-label='Dialpad' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>dialpad</i>
-                            <i aria-label='Check Box' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>check_box</i>
-                            <i aria-label='Radio Button Checked' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>radio_button_checked</i>
-                            <i aria-label='List Alt' className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>list_alt</i>
+                            <i onClick={() => setArray([...array, <TextInput label='שם משתמש' />])}
+                                aria-label='Text Fields'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>text_fields
+                            </i>
+                            <i onClick={() => setArray([...array, <NumberInput />])}
+                                aria-label='Pin'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>pin
+                            </i>
+                            <i onClick={() => setArray([...array, <Textarea />])}
+                                aria-label='Segment'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>segment
+                            </i>
+                            <i onClick={() => setArray([...array, <Button />])}
+                                aria-label='Dialpad'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>dialpad
+                            </i>
+                            <i onClick={() => setArray([...array, <Checkbox />])}
+                                aria-label='Check Box'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>check_box
+                            </i>
+                            <i onClick={() => setArray([...array, <Radio />])}
+                                aria-label='Radio Button Checked'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>radio_button_checked
+                            </i>
+                            <i onClick={() => setArray([...array, <Select />])}
+                                aria-label='List Alt'
+                                className='material-icons  text-gray-500 mx-2 cursor-pointer hover:text-gray-400 text-2xl'>list_alt
+                            </i>
 
                         </div>)}
                     </div>
