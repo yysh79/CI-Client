14bc4d12bb304c341d9047ad59e34412c594ef0e
Shalom Gispan <shalomgispan1@gmail.com>
2024-10-31 12:46:55 +0200

    nice

--- src/components/Delete.tsx
+++ src/components/Delete.tsx
@@ -5,7 +5,6 @@ interface ConfirmationDialogueProps {
 }
 
 const Delete: React.FC<ConfirmationDialogueProps> = ({ userId, leave }) => {
-
     const deleteUser = async () => {
         try {
             const response = await fetch(`http://localhost:3000/users/deleteUser/${userId}`, {
@@ -19,7 +18,6 @@ const Delete: React.FC<ConfirmationDialogueProps> = ({ userId, leave }) => {
             console.error(error)
         }
     };
-
     return (
         <>
             <button onClick={deleteUser} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,41 +1,22 @@
 import React from 'react';
 import { RiFileExcel2Line } from "react-icons/ri";
-
 const ExportButton: React.FC = () => {
     const handleExport = async () => {
         try {
             const response = await fetch('http://localhost:3000/users/exportToExcelAllUsers', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                },
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            // קבלת הקובץ
+                method: 'GET',headers: {'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',},});
+            if (!response.ok) {throw new Error('Network response was not ok');}
             const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
-
-            // יצירת קישור להורדה
             const a = document.createElement('a');
             a.href = url;
-            a.download = 'users.xlsx'; // שם הקובץ 
+            a.download = 'users.xlsx'; 
             document.body.appendChild(a);
-            a.click(); // לחיצה על הקישור להורדה
-            a.remove(); // הסרת הקישור מה-DOM
-
-        } catch (error) {
-            console.error('Error exporting users:', error);
-        }
-    };
-
+            a.click();
+            a.remove(); 
+        } catch (error) {console.error('Error exporting users:', error);}};
     return (
         <button onClick={handleExport}
-         className=" bg-gray-500 flex text-white font-bold py-2 px-4 rounded hover:text-gray-300 items-center gap-5 transition duration-300">ייצוא לאקסל<RiFileExcel2Line size={20} /></button>
-    );
-};
-
+         className=" bg-gray-500 flex text-white font-bold py-2 px-4 rounded hover:text-gray-300 items-center gap-5 transition duration-300"
+         >ייצוא לאקסל<RiFileExcel2Line size={20} /></button>);};
 export default ExportButton;
